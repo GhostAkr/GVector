f3accc96e8e38a5577dbcf342f621e8a8e620305
Alexander Antoshkin <ighost99@gmail.com>
2023-11-05 17:58:30 +0300

    Template GVector

---
+++ inc/gvector.h
@@ -0,0 +1,143 @@
+#ifndef GVECTOR_HPP
+#define GVECTOR_HPP
+
+#include <cstddef>
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <stdexcept>
+
+namespace GAlg
+{
+    // Forward declarations
+
+    template<typename T>
+    class GVector;
+
+    template<typename T>
+    void swap(GVector<T>& first, GVector<T>& second);
+
+    // Classes
+
+    template<typename T>
+    class GVector
+    {
+    public:
+
+        // Structors
+
+        GVector() noexcept
+            : _val(nullptr)
+            , _size(0)
+        {}
+
+        GVector(std::size_t size)
+            : _val(new int[size])
+            , _size(size)
+        {}
+
+        GVector(const GVector& other)
+            : GVector(other._size)
+        {
+            std::memcpy(this->_val, other._val, other._size * sizeof(int));
+        }   
+
+
+        GVector(GVector&& other) noexcept
+            : _val(other._val)
+            , _size(other._size)
+        {
+            other._val = nullptr;
+            other._size = 0;
+        }
+
+        ~GVector()
+        {
+            delete[] _val;
+        }
+
+        // Operators
+
+        GVector& operator=(GVector other)
+        {
+            GAlg::swap<T>(*this, other);
+            return *this;
+        }
+
+        T& operator[](size_t ix)
+        {
+            if (ix + 1 > this->_size)
+            {
+                std::string errorMsg = std::string("Trying to get element with index ") + 
+                    std::to_string(ix) + std::string(" from array with size ") + 
+                    std::to_string(this->_size);
+
+                throw std::out_of_range(errorMsg);
+            }
+
+            return this->_val[ix];
+        }
+
+        // Friends
+
+        template<typename A>
+        friend void GAlg::swap(GVector<A>& firstm, GVector<A>& second);
+
+        // Methods
+
+        void print()
+        {
+            if (this->_val == nullptr || this->_size <= 0)
+            {
+                std::string errorMsg = std::string("Trying to print uninitialized GVector");
+                
+                throw std::runtime_error(errorMsg);
+            }
+
+            int maxNumsInRow = 10;
+            int numsInRow = 0;
+            for (size_t ix = 0; ix < this->_size; ++ix)
+            {
+                std::cout << this->_val[ix];
+                numsInRow++;
+
+                if (ix != this->_size - 1)
+                    std::cout << " ";
+
+                if (numsInRow == maxNumsInRow || ix == this->_size - 1)
+                {
+                    std::cout << std::endl;
+                    numsInRow = 0;
+                }
+            }
+        }
+
+    private:
+
+        // Fields
+
+        int* _val;
+        std::size_t _size;
+
+        // Methods
+
+        void swap(GVector& other)
+        {
+            using std::swap;
+
+            swap(this->_val, other._val);
+            swap(this->_size, other._size);
+        }
+
+    };
+
+    // GAlg's functions
+
+    template<typename T>
+    void swap(GVector<T>& first, GVector<T>& second)
+    {
+        first.swap(second);
+    }
+}
+
+#endif  // GVECTOR_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 
-#include "../inc/gvector.hpp"
+#include "../inc/gvector.h"
 
 int main()
 {
     std::cout << "Starting app" << std::endl;
-    GAlg::GVector vec(12);
+    GAlg::GVector<int> vec(12);
 
     vec[0] = 4; vec[1] = 3; vec[2] = 14; vec[3] = 3; vec[4] = 312; vec[5] = 21;
     vec[6] = 3; vec[7] = 56; vec[8] = 34; vec[9] = 67; vec[10] = 22; vec[11] = 44;
@@ -13,14 +13,12 @@ int main()
     std::cout << "Initial GVector" << std::endl;
     vec.print();
 
-    GAlg::GVector vecCopy;
-    vecCopy = vec;
-    
+    GAlg::GVector vecCopy = vec;
+
     std::cout << "Copied GVector" << std::endl;
     vecCopy.print();
 
-    GAlg::GVector vecMove;
-    vecMove = std::move_if_noexcept(vecCopy);
+    GAlg::GVector vecMove = std::move_if_noexcept(vecCopy);
 
     std::cout << "Moved GVector" << std::endl;
     vecMove.print();
